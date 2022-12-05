6c5eabe6ee13a9b80100243bc6c419a159070a5f
erin petra sofiya moon <erin@hecke.rs>
2022-12-05 02:46:58 -0600

    showdefect util

---
+++ ariadne/util/__init__.py
@@ -0,0 +1,20 @@
+from .deeppcb import Defect
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def showdefects(img, defects: list[Defect]):
+    """
+    show the given list of defects superimposed on top of an image.
+    """
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.imshow(img)
+    for defect in defects:
+        rect = patches.Rectangle((defect.x0,defect.y0), defect.width, defect.height,
+                                facecolor='none', edgecolor='r')
+        ax.add_patch(rect)
+        ax.text(defect.x0, defect.y0-5, f'{defect.ty.name}', color='r')
+
+    return fig, ax
+
