ec6090e9394e5e4e8d5fdb04d4ee0d54a3052ab4
erin petra sofiya moon <erin@hecke.rs>
2022-12-11 21:07:46 -0600

    txform

--- ariadne/nn/transform.py
+++ ariadne/nn/transform.py
@@ -1,3 +1,7 @@
+import torch
+from torch import nn, Tensor
+import torchvision.transforms.functional as F
+
 class Compose:
     def __init__(self, transforms):
         self.transforms = transforms
@@ -11,8 +15,8 @@ class PILToTensor(nn.Module):
     def forward(
         self, tmpl_image, obsv_image, target,
     ):
-        tmpl_image = tvtF.pil_to_tensor(tmpl_image)
-        obsv_image = tvtF.pil_to_tensor(obsv_image)
+        tmpl_image = F.pil_to_tensor(tmpl_image)
+        obsv_image = F.pil_to_tensor(obsv_image)
         return tmpl_image, obsv_image, target
 
 class ConvertImageDtype(nn.Module):
@@ -23,6 +27,6 @@ class ConvertImageDtype(nn.Module):
     def forward(
         self, tmpl_image, obsv_image, target,
     ):
-        tmpl_image = tvtF.convert_image_dtype(tmpl_image, self.dtype)
-        obsv_image = tvtF.convert_image_dtype(obsv_image, self.dtype)
+        tmpl_image = F.convert_image_dtype(tmpl_image, self.dtype)
+        obsv_image = F.convert_image_dtype(obsv_image, self.dtype)
         return tmpl_image, obsv_image, target
